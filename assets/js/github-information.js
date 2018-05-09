f8f2af27f65ac0aeceff5c5d48ba185c50950592
Colman Leung <comacoma0218@hotmail.com>
2018-05-09 13:37:52 +0100

    add user friendly message for API throttling

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -16,17 +16,17 @@ function userInformationHTML(user) {
 }
 
 function repoInformationHTML(repos) {
-    if (repos.length == 0) {
-        return `<div class="clearfix repo-list">No repos!</div>`;
-    }
+  if (repos.length == 0) {
+    return `<div class="clearfix repo-list">No repos!</div>`;
+  }
 
-    var listItemsHTML = repos.map(function(repo) {
-        return `<li>
+  var listItemsHTML = repos.map(function(repo) {
+    return `<li>
                     <a href="${repo.html_url}" target="_blank">${repo.name}</a>
                 </li>`;
-    });
+  });
 
-    return `<div class="clearfix repo-list">
+  return `<div class="clearfix repo-list">
                 <p>
                     <strong>Repo List:</strong>
                 </p>
@@ -52,7 +52,8 @@ function fetchGitHubInformation(event) {
         </div>`);
 
   $.when(
-    $.getJSON(`https://api.github.com/users/${username}`), $.getJSON(`https://api.github.com/users/${username}/repos`)
+    $.getJSON(`https://api.github.com/users/${username}`),
+    $.getJSON(`https://api.github.com/users/${username}/repos`)
   ).then(
     function(firstResponse, secondResponse) {
       var userData = firstResponse[0];
@@ -64,6 +65,9 @@ function fetchGitHubInformation(event) {
       if (errorResponse.status === 404) {
         $("#gh-user-data").html(
           `<h2>No info found for user ${username}</h2>`);
+      } else if (errorResponse.status === 403) {
+        var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+        $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
       } else {
         console.log(errorResponse);
         $("#gh-user-data").html(
